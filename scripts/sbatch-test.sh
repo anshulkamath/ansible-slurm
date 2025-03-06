3f0fb0b1a5a5040ebb2843c2d46915e463241d4d
Anshul Kamath <anshulkam@gmail.com>
2025-03-06 15:30:54 -0500

    nfs: standardize mounts between client and server

--- scripts/sbatch-test.sh
+++ scripts/sbatch-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=gpu_test
-#SBATCH --output=/mnt/slurm/slurm-%j.out
+#SBATCH --output=/mnt/ubuntu/slurm-%j.out
 #SBATCH --nodes=2
 #SBATCH --gres=gpu:1   # Request 1 GPU per node
 #SBATCH --ntasks-per-node=1
